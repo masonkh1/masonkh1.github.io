112451ac3904c26c4122395de9fa12b13f204140
masonkh1 <masonkh@hotmail.com>
2022-04-23 17:27:00 -0600

    Added art made refinements.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # masonkh1.github.io
 Mason Hall
+(5 points) make the menu sticky: The menu should remain attached to the top of the page as the user scrolls down.
+(5 points) smooth scrolling: go to google and look up how to do smooth scrolling in CSS, so that when the user clicks on your page’s menu, the page will smoothly scroll to the section instead of snapping instantly to the section. 
+(10 points) auto-highlight menu: The menu should highlight each item as the user scrolls through that section, and as a section scrolls up past the top of the viewport, the menu item for that section should become highlighted,
+(5 points) Parallax background
---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navlink");
+const projectButtons = document.querySelectorAll(".project-button");
+const projectContents = document.querySelectorAll(".project-content");
+
+window.addEventListener('scroll', function() {
+    let current = ""
+    for (var section of sections) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(scrollY > sectionTop - 100){
+            current = section.getAttribute('id');
+        }
+    }
+    for (var navLink of navLinks) {
+        navLink.classList.remove('active');
+    }
+    if (current) {
+        this.document.getElementById(current + "-nav").classList.add('active');
+    }
+})
+
+
+for (i = 0; i < projectButtons.length; i++) {
+    
+    projectButtons[i].addEventListener("click", function() {
+        var id = this.id + "-content";
+        var projectContent = document.getElementById(id);
+        //Resets other element's heights
+        for (element of projectContents) {
+            if (element.id !== id) {
+                element.style.maxHeight = null;
+            }
+        }
+        //Sets max height of content element
+        if (projectContent.style.maxHeight) {
+            projectContent.style.maxHeight = null;
+        } else {
+            projectContent.style.maxHeight = projectContent.scrollHeight + "px";
+        }
+    } );
+}
